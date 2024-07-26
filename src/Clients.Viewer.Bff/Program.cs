1f21155a4137fc2217552e572846e66be87e403c
Oscar Veldman <oveldman@gmail.com>
2024-07-26 16:33:36 +0200

    Refactor small Sonarqube feedback

--- src/Clients.Viewer.Bff/Program.cs
+++ src/Clients.Viewer.Bff/Program.cs
@@ -1,3 +1,5 @@
+using MadWorldNL.MantaRayPlan;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -35,8 +37,3 @@ app.MapGet("/weatherforecast", () =>
     .WithOpenApi();
 
 await app.RunAsync();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
