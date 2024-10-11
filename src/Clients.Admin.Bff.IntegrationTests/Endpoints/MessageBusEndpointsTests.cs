a162c99f8cab4802a2fbdb1f37bb142005e2cafc
Oscar Veldman <oveldman@gmail.com>
2024-10-11 09:18:00 +0200

    Remove weather

---
+++ src/Clients.Admin.Bff.IntegrationTests/Endpoints/MessageBusEndpointsTests.cs
@@ -0,0 +1,16 @@
+namespace MadWorldNL.MantaRayPlan.Endpoints;
+
+public class MessageBusEndpointsTests
+{
+    [Fact]
+    public void GetStatus_WhenSendWithoutParameters_ShouldReturnCurrentCounter()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Fact]
+    public void PostStatus_WhenSendWithoutParameters_ShouldIncreaseCounter()
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Clients.Viewer.Bff/Program.cs
+++ src/Clients.Viewer.Bff/Program.cs
@@ -62,24 +62,8 @@ app.UseSwaggerUI();
 
 app.MapHealthChecks("/healthz");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
+app.MapGet("/TODO", () => "NotImplemented")
+    .WithName("TODO")
     .WithOpenApi()
     .RequireRateLimiting(RateLimiterConfig.DefaultName);
 
